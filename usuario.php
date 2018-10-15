b842066d84bc3b461746c99bf77b76906dcd6904
jpcastanod <30413807+jpcastanod@users.noreply.github.com>
2018-10-15 17:29:37 -0500

    Update usuario.php

--- usuario.php
+++ usuario.php
@@ -67,6 +67,7 @@
 
       if(isset($_POST['btn_consultar']))
       {
+        
         $nombre = $_POST ['nombre'];
         $autor = $_POST ['autor'];
 
@@ -83,6 +84,58 @@
             <td><?php echo $consulta['editorial']. "<br>"; ?> </td>
             <td><?php echo $consulta['anio']. "<br>"; ?> </td>
             <td><?php echo $consulta['estanteria']. "<br>"; ?></td>
+            <?php 
+            if ($estanteria >= 1 and $estanteria <=3){
+              ?>
+              <td><?php echo "<a href='http://localhost/integrador/1a3.png'>Localizar</a>". "<br>"; ?></td>
+              <?php
+            }
+            ?>
+            <?php 
+            if ($estanteria >= 4 and $estanteria <=5){
+              ?>
+              <td><?php echo "<a href='http://localhost/integrador/3a5.png'>Localizar</a>". "<br>"; ?></td>
+              <?php
+            }
+            ?>
+            <?php 
+            if ($estanteria >= 6 and $estanteria <=9){
+              ?>
+              <td><?php echo "<a href='http://localhost/integrador/6a9.png'>Localizar</a>". "<br>"; ?></td>
+              <?php
+            }
+            ?>
+            <?php 
+            if ($estanteria >= 10 and $estanteria <=12){
+              ?>
+              <td><?php echo "<a href='http://localhost/integrador/10a12.png'>Localizar</a>". "<br>"; ?></td>
+              <?php
+            }
+            ?>
+            <?php 
+            if ($estanteria >= 13 and $estanteria <=15){
+              ?>
+              <td><?php echo "<a href='http://localhost/integrador/13a15.png'>Localizar</a>". "<br>"; ?></td>
+              <?php
+            }
+            ?>
+            <?php 
+            if ($estanteria >= 16 and $estanteria <=18){
+              ?>
+              <td><?php echo "<a href='http://localhost/integrador/16a18.png'>Localizar</a>". "<br>"; ?></td>
+              <?php
+            }
+            ?>
+            <?php 
+            if ($estanteria >= 19 and $estanteria <=21){
+              ?>
+              <td><?php echo "<a href='http://localhost/integrador/6a9.png'>Localizar</a>". "<br>"; ?></td>
+              <?php
+            }
+            ?>
+
+            
+
 
 
             </tr>
